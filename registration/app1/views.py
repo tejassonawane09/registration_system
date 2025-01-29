2344d84673a8f0fb8de9d92070fbe3ef0fece351
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-29 15:54:00 +0530

    login

--- registration/app1/views.py
+++ registration/app1/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,6 +8,21 @@ def HomePage(request):
 
 
 def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        
+        if pass1 != pass2:
+            return HttpResponse("Your Password & Confirm Password Are Not Same!!!")
+        else:            
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+        #return HttpResponse("User has been created sucessfully!!!")
+        
+        print(uname,email,pass1,pass2)
     return render(request,'signup.html')
     
 
