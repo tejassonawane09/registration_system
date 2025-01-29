3b0c36ac5e860feede802edd6e5d14fc0d76f766
Tejas Sonawane <tejassonawane3109@gmail.com>
2025-01-29 22:14:34 +0530

    final

--- registration/app1/views.py
+++ registration/app1/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required(login_url='login')
 def HomePage(request):
     return render(request,'home.html')
 
